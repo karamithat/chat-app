55a717b8f0684fba3012ae3df431e30dc36d831c
karamithat <mithatkara93@gmail.com>
2025-01-02 23:21:20 +0300

    add controller and new model

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,9 @@
 import express from "express";
 import authRoutes from "./src/routes/auth.route.js";
+import messageRoutes from "./src/routes/message.route.js";
 import dotenv from "dotenv";
 import { connectDb } from "./src/lib/db.js";
+import cookieParser from "cookie-parser";
 
 const app = express();
 dotenv.config();
@@ -9,8 +11,10 @@ dotenv.config();
 const PORT = process.env.PORT;
 
 app.use(express.json());
+app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
+app.use("/api/message", messageRoutes);
 
 app.listen(PORT, () => {
   console.log(`server is running on port ${PORT}`);
