ef176cf79121e1e0109f431db2603dfd672224e6
karamithat <mithatkara93@gmail.com>
2025-01-07 23:43:12 +0300

    implementing socket.io

--- backend/index.js
+++ backend/index.js
@@ -5,8 +5,8 @@ import dotenv from "dotenv";
 import { connectDb } from "./src/lib/db.js";
 import cookieParser from "cookie-parser";
 import cors from "cors";
+import { app, server } from "./src/lib/socket.js";
 
-const app = express();
 dotenv.config();
 
 const PORT = process.env.PORT;
@@ -18,7 +18,7 @@ app.use(cors({ origin: "http://localhost:5173", credentials: true }));
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
-app.listen(PORT, () => {
+server.listen(PORT, () => {
   console.log(`server is running on port ${PORT}`);
   connectDb();
 });
